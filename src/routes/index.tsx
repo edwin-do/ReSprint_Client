8ef77987e941b0df4451cb653f7ccfa8938f5453
Edwin Do <doe@mcmaster.ca>
2023-03-17 03:27:43 -0400

    add test controls for signalr

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,45 @@
-import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+import { createSignal, createEffect } from "solid-js";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 
 export default function Home() {
+  const [messages, setMessages] = createSignal([]);
+
+  let connection;
+
+  createEffect(() => {
+    connection = new HubConnectionBuilder()
+      .withUrl("https://differentgoldlamp8.conveyor.cloud/Hubs/chatHub")
+      .withAutomaticReconnect()
+      .build();
+
+    connection
+      .start()
+      .then(() => console.log("SignalR Connected"))
+      .catch((err) => console.log("SignalR Connection Error: ", err));
+
+    connection.on("ReceiveMessage", (message) =>
+      setMessages((prevMessages) => [...prevMessages, message])
+    );
+    
+  });
+
+  const handleClick = () => {
+    const message = document.getElementById("message").value;
+    console.log(message);
+    connection.invoke("sendMessage", message);
+    document.getElementById("message").textContent = "";
+  };
+
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <h1>Chat App</h1>
+      <input type="text" id="message" placeholder="Type a message"/>
+      <button id="send" onClick={handleClick}>Send</button>
+      <ul>
+        {messages().map((message, index) => (
+          <li>{message}</li>
+        ))}
+      </ul>
     </main>
   );
 }
